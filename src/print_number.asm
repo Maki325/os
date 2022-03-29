af9bc94d6606fc6d42d8e23d3bb45eb34df099a0
Marko <markoni521@gmail.com>
2022-03-29 23:08:20 +0200

    `printNumber` function now prints the number the right way

--- src/print_number.asm
+++ src/print_number.asm
@@ -5,16 +5,19 @@ printNumber:
   printNumberDigit:
     mov bl, 10
     div bl ; Quotient: `al`, Remainder: `ah`
-    mov bh, al
-    mov al, ah
+    mov bl, ah
+
+    mov ah, 0
+    cmp ax, 0
+    jz printNumberDigitP2
+    call printNumber
+    printNumberDigitP2:
+
+    mov al, bl
     add al, '0'
     mov ah, 0x0e
+
     int 0x10
-    mov ah, 0
-    mov al, bh
-    cmp ax, 0
-    jz printNumberEnd
-    jmp printNumberDigit
 
   printNumberEnd:
     popa
