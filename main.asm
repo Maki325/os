dc6e4f1c705d516e90c1eb95b9a2fc8835a2d1c3
Marko <markoni521@gmail.com>
2022-03-29 22:31:40 +0200

    Added word printing, and number printing (in reverse sadly)

--- main.asm
+++ main.asm
@@ -1,17 +1,23 @@
 [org 0x7c00]
-; A simple boot sector program that loops forever
-; mov al, "O"
-; call printChar
-; mov al, 0x0a
-; call printChar
 
-; mov al, [wordLen]
+mov bx, toPrint
+mov dh, [wordLen]
+call printWord
+
+mov ax, 134
+call printNumber
+
+; mov bp, 0x8000 ; this is an address far away from 0x7c00 so that we don't get overwritten
+; mov sp, bp ; if the stack is empty then sp points to bp
+
+; mov al, sp
 ; add al, '0'
 ; call printChar
 
-mov bx, toPrint
-mov dx, [wordLen]
-call printWord
+; push 'A'
+; push 'B'
+; push 'C'
+
 
 jmp $
 
@@ -20,30 +26,46 @@ jmp $
 printChar:
   mov ah, 0x0e
   int 0x10
-  ret
+ret
 
 printWord:
   pusha
-  mov BYTE [wordCount], 0
   mov ah, 0x0e
   printWordLetter:
-    cmp [wordCount], dx
+    cmp dh, 0
     jz printWordEnd
     mov al, [bx]
     int 0x10
-    mov al, [wordCount]
-    inc al
-    mov [wordCount], al
+    dec dh
     add bx, 1
+    jmp printWordLetter
 
   printWordEnd:
-      mov al, [wordCount]
-      add al, '0'
-      call printChar
     popa
-    ret
+ret
 
-toPrint: db 'Hello, World!'
+; Put number into `ax`
+printNumber:
+  pusha
+  printNumberDigit:
+    mov bl, 10
+    div bl ; Quotient: `al`, Remainder: `ah`
+    mov bh, al
+    mov al, ah
+    add al, '0'
+    mov ah, 0x0e
+    int 0x10
+    mov ah, 0
+    mov al, bh
+    cmp ax, 0
+    jz printNumberEnd
+    jmp printNumberDigit
+
+  printNumberEnd:
+    popa
+ret
+
+toPrint: db 'Hello, World!', 10, 0x0D
 wordLen: db $-toPrint
 wordCount: db 0
 times 510-($-$$) db 0
